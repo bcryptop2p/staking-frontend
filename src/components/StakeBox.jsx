8c7bbb747fd1db1aa776ec482cf47fcf04da20fe
zetazeros <zetazeros@protonmail.com>
2022-07-21 22:10:24 +0100

    clean

--- src/components/StakeBox.jsx
+++ src/components/StakeBox.jsx
@@ -5,7 +5,6 @@ import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
