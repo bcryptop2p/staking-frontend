68cca04e380b27a28b8399af19d23ab1da601ea0
zetazeros <zetazeros@protonmail.com>
2022-07-21 03:02:04 +0100

    DataTable refresh

--- src/components/StakeBox.jsx
+++ src/components/StakeBox.jsx
@@ -2,7 +2,26 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Box, Container } from '@mui/system';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 import '../style/App.css';
+
+function createData(name, calories) {
+  return { name, calories };
+}
+
+const rows = [
+  createData("You will recieve", 159),
+  createData("Exchange rate", 237),
+  createData("Transaction cost", 262),
+  createData("Reward fee", 305)
+];
 
 
 export default function StakeBox() {
@@ -10,9 +29,36 @@ export default function StakeBox() {
     <React.Fragment>
       <CssBaseline />
       <Container className="staker" maxWidth="sm">
-        <h5 className="stakeEth">Stake ETH and receive stETH while staking.</h5>
-        <Box sx={{ bgcolor: '#fff', height: '60vh', boxShadow: 'rgba(17, 12, 46, 0.15) 0px 48px 100px 0px', borderRadius: '25px', marginTop: '-5px', paddingTop: '50px'}}>
-        <TextField className="amount" id="outlined-basic" label="Amount" variant="outlined" />
+        <Box>
+        <h5 className="stakeEth">Stake Ether</h5>
+        <p>Stake ETH and receive stETH while staking.</p>
+        </Box>
+        <Box sx={{ bgcolor: '#fff', height: '50vh', boxShadow: 'rgba(17, 12, 46, 0.15) 0px 48px 100px 0px', borderRadius: '25px', marginTop: '-5px', paddingTop: '50px'}}>
+        <Stack component="form" sx={{ width: '80%', margin: 'auto' }} >
+        <TextField className="amount" id="outlined-basic" label="Amount" variant="outlined" sx={{marginBottom: '30px'}} />
+        <Button variant="contained" size="large">
+          Submit
+        </Button>
+        </Stack>
+
+        <TableContainer>
+          <Table sx={{ width: '80%', margin: 'auto', borderBottom: '0', marginTop: '25px' }} aria-label="simple table">
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{  borderBottom: '0'  }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
         </Box>
       </Container>
     </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
+  </React.StrictMode>
 );
-
-reportWebVitals(sendToVercelAnalytics);
